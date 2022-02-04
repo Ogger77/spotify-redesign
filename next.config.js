e00ba59c9b27a35278454ca78bf5af5767ca429b
Dinh Vu <vud@kean.edu>
2022-02-03 22:44:01 -0500

    initialize

---
+++ next.config.js
@@ -0,0 +1,5 @@
+module.exports = {
+  images: {
+    domains: ["rb.gy", "1000logos.net"],
+  },
+};
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,40 @@
 module.exports = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx}',
-    './components/**/*.{js,ts,jsx,tsx}',
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      screen: {
+        "2xl": "1500px",
+      },
+      keyframes: {
+        pulse: {
+          "0%, 100%": {
+            opacity: 1,
+          },
+          "50%": {
+            opacity: 0.5,
+          },
+        },
+      },
+      animation: {
+        pulse: "pulse 4s cubic-bezier(0.4, 0, 0.6, 1) infinite",
+      },
+    },
+
+    fontFamily: {
+      body: ["Andada Pro", "serif"],
+      sans: ["ui-sans-serif", "system-ui"],
+    },
   },
-  plugins: [],
-}
+  variants: {
+    extend: {},
+    scrollbar: ["rounded"],
+  },
+  plugins: [
+    require("tailwind-scrollbar-hide"),
+    require("tailwind-scrollbar"),
+    require("@tailwindcss/forms"),
+  ],
+};
